1a4e66371ec7a6fa36f441b58f49e883199a5f7a
Merdi-Elongo <54002007+Merdi-Elongo@users.noreply.github.com>
2020-05-13 18:45:24 +0100

    Update MainActivity.java

--- app/src/main/java/com/safari/demo_android/MainActivity.java
+++ app/src/main/java/com/safari/demo_android/MainActivity.java
@@ -1,7 +1,9 @@
 package com.safari.demo_android;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,6 +11,9 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.zxing.BarcodeFormat;
 import com.mobiwire.CSAndroidGoLib.AndroidGoCSApi;
 import com.mobiwire.CSAndroidGoLib.CsPrinter;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,22 +31,29 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     public void print(View view){
 
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
         String sTitle = title.getEditText().getText().toString().trim();
         String sName = name.getEditText().getText().toString().trim();
         String sFirst = first.getEditText().getText().toString().trim();
 
         CsPrinter printer = new CsPrinter();
         printer.addTextToPrint(sTitle, null, 50, true, false, 1);
-        printer.addTextToPrint("av. frontier N 163 C/kalamu Q/Kauka", "",35, false, false, 1);
-        printer.addTextToPrint("+243825341040", "", 30, false, false, 1);
-        printer.addTextToPrint("02.10.2020", "expire le 05.10.2020 14:00", 20, true, false, 3);
-        printer.addTextToPrint("", "", 30, false, false, 1);
+        printer.addTextToPrint("av. frontier N 163 C/kalamu Q/Kauka", null,20, true, false, 1);
+        printer.addTextToPrint("+243825341040", null, 24, false, true, 1);
+        printer.addTextToPrint(dateTimeFormatter.format(now), "B613", 24, true, false, 3);
         printer.addTextToPrint("- - - - - - - - - - - - - - - - - - - - - - - - ", "", 30, false, false, 1);
-        printer.addTextToPrint("Noms ", sName + " "+sFirst, 20, false, false, 8);
-        printer.addTextToPrint("Trajet: " ,"KIN-MATADI", 20, false,  false, 8);
-        printer.addTextToPrint("date voyage : ", "28.05.2020", 20, false, false, 8);
+        printer.addTextToPrint("Noms ", sName + " "+sFirst, 24, true, false, 8);
+        printer.addTextToPrint("Trajet: " ,"KIN-MATADI", 24, true,  false, 8);
+
+        printer.addTextToPrint("- - - - - - - - - - - - - - - - - - - - - - - - ", "", 30, false, false, 1);
+        printer.addTextToPrint("date voyage : ", "28.05.2020", 24, true, false, 8);
+        printer.addTextToPrint("depart : ", "07h30", 24, true, false, 8);
+        printer.addTextToPrint("expire le 20.05.2020 14:00", null, 24, true, false, 3);
+        printer.addTextToPrint("", "", 30, true, false, 1);
         printer.addBarQrCodeToPrint(sTitle+sName+sFirst, BarcodeFormat.QR_CODE, 384, 280);
         printer.print(this);
     }
